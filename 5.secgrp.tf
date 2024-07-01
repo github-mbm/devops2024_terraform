b2efdcff8828c14f128fb3c7aa37838189c0b102
root <root@ip-10-90-1-28.ap-south-1.compute.internal>
2024-07-01 10:44:17 +0000

    secgrp https port opened

--- 5.secgrp.tf
+++ 5.secgrp.tf
@@ -9,6 +9,13 @@ resource "aws_security_group" "allow_all_secgrp" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    description = "TLS from HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port   = 0
