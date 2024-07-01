b1c6a7d8813f6c5b2b196fcfff0db57f51a136ee
mbm <daffodilss19@gmail.com>
2024-07-01 16:08:13 +0530

    developed the security grp

---
+++ 5.secgrp.tf
@@ -0,0 +1,23 @@
+resource "aws_security_group" "allow_all_secgrp" {
+  name   = "allow_all_traffic"
+  vpc_id = aws_vpc.vpc1.id
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.vpc1_name}-SG"
+  }
+}
